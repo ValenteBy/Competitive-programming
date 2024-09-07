80d8ef0edfee340b76730ab5b84b4ea476cacdc1
ValenteBy <samuel.valente188@gmail.com>
2024-09-07 10:19:11 -0300

    Add Zeros e uns

---
+++ maratona/2023/Investigando_Zeros_e_Uns.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define in insert
+#define llv vector<ll>
+#define int ll
+#define DEBUG std::cout << "DEBUG" << std::endl;
+#define _ ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+using namespace std;
+
+signed main(){ _
+    
+    DEBUG
+
+    return 0;
+}
